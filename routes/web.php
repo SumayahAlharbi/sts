afa3990f03adafe3a0ee6824724c20d1c3c21212
Ssy3 <ssy.vip@gmail.com>
2018-10-30 09:40:09 +0300

    All routs ->middleware('auth')

--- routes/web.php
+++ routes/web.php
@@ -22,26 +22,26 @@ Route::get('/home', 'HomeController@index')->name('home');
 //Tickets Routes
 Route::resource('ticket','TicketController')->middleware('auth');
 //category Routes
-Route::resource('category','CategoryController');
+Route::resource('category','CategoryController')->middleware('auth');
 //location Routes
-Route::resource('location','LocationController');
+Route::resource('location','LocationController')->middleware('auth');
 //Status route
-Route::resource('status','StatusController');
+Route::resource('status','StatusController')->middleware('auth');
 
 // assign agent to a ticket
-Route::post('ticket/addTicketAgent','TicketController@addTicketAgent');
+Route::post('ticket/addTicketAgent','TicketController@addTicketAgent')->middleware('auth');
 // remove agent from a ticket
-Route::get('ticket/removeTicketAgent/{user_id}/{ticket_id}','\App\Http\Controllers\TicketController@removeTicketAgent');
+Route::get('ticket/removeTicketAgent/{user_id}/{ticket_id}','\App\Http\Controllers\TicketController@removeTicketAgent')->middleware('auth');
 
 //Groups Routes
-Route::resource('group','GroupController');
+Route::resource('group','GroupController')->middleware('auth');
 
-Route::resource('permissions','PermissionController');
-Route::resource('roles','RoleController');
+Route::resource('permissions','PermissionController')->middleware('auth');
+Route::resource('roles','RoleController')->middleware('auth');
 
 
 //roles has permissions Routes
 Route::group(['middleware'=> 'web'],function(){
-  Route::post('roles/addPermission','\App\Http\Controllers\RoleController@addPermission');
-  Route::get('roles/removePermission/{permission}/{role_id}','\App\Http\Controllers\RoleController@revokePermission');
+  Route::post('roles/addPermission','\App\Http\Controllers\RoleController@addPermission')->middleware('auth');
+  Route::get('roles/removePermission/{permission}/{role_id}','\App\Http\Controllers\RoleController@revokePermission')->middleware('auth');
 });
