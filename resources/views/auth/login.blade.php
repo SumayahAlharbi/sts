ae30c6689fb2f18b9ce8e68cf23af3d83be388d9
omego <mserihi@gmail.com>
2018-11-20 16:30:10 +0300

    Theming init

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,76 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+  <div class="login-register" style="background-image:url({{ url('assets/images/background/login-register-2.jpg') }});">
+      <div class="login-box card">
+          <div class="card-body">
+              <form method="POST" class="form-horizontal form-material" id="loginform" action="{{ route('login') }}">
+                @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                  <h3 class="box-title m-b-20">{{ __('Login') }}</h3>
+                  <div class="form-group ">
+                      <div class="col-xs-12">
+                          {{-- <input class="form-control" type="text" required="" placeholder="Username"> --}}
+                          <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                          @if ($errors->has('email'))
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $errors->first('email') }}</strong>
+                              </span>
+                          @endif
+                         </div>
+                  </div>
+                  <div class="form-group">
+                      <div class="col-xs-12">
+                          {{-- <input class="form-control" type="password" required="" placeholder="Password"> --}}
+                          <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                          @if ($errors->has('password'))
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $errors->first('password') }}</strong>
+                              </span>
+                          @endif
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                         </div>
+                  </div>
+                  <div class="form-group">
+                      <div class="col-md-12">
+                          <div class="checkbox checkbox-primary pull-left p-t-0">
+                              <input id="checkbox-signup" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                              <label for="checkbox-signup"> {{ __('Remember Me') }} </label>
+                          </div> <a href="javascript:void(0)" id="to-recover" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> {{ __('Forgot Your Password?') }}</a> </div>
+                  </div>
+                  <div class="form-group text-center m-t-20">
+                      <div class="col-xs-12">
+                          <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">{{ __('Login') }}</button>
+                      </div>
+                  </div>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                  <div class="form-group m-b-0">
+                      <div class="col-sm-12 text-center">
+                          <p>Don't have an account? <a href="{{ route('register') }}" class="text-info m-l-5"><b>Sign Up</b></a></p>
+                      </div>
+                  </div>
+              </form>
+              <form class="form-horizontal" id="recoverform" action="index.html">
+                  <div class="form-group ">
+                      <div class="col-xs-12">
+                          <h3>Recover Password</h3>
+                          <p class="text-muted">Enter your Email and instructions will be sent to you! </p>
+                      </div>
+                  </div>
+                  <div class="form-group ">
+                      <div class="col-xs-12">
+                          <input class="form-control" type="text" required="" placeholder="Email"> </div>
+                  </div>
+                  <div class="form-group text-center m-t-20">
+                      <div class="col-xs-12">
+                          <button class="btn btn-primary btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Reset</button>
+                      </div>
+                  </div>
+              </form>
+          </div>
+      </div>
+  </div>
 @endsection
--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -1,15 +1,6 @@
-@extends('layouts.app')
+@extends('layouts.material')
 
 @section('content')
-
-<div class = 'container'>
-  @role('admin')
-<div class="row">
-<div class="col">
-<a class="btn btn-primary" href="{{ route('ticket.create')}}" role="button">New  <i class="icon ion-md-add-circle"></i></a>
-</div>
-</div>
-@endrole
 
 @if(session()->get('success'))
   <div class="alert alert-success">
@@ -22,8 +13,115 @@
   </div><br />
 @endif
 
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Support Ticket List</h4>
+                <h6 class="card-subtitle">List of ticket opend by customers</h6>
+                <div class="row m-t-40">
+                    <!-- Column -->
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-inverse card-info">
+                            <div class="box bg-info text-center">
+                                <h1 class="font-light text-white">{{$tickets->count()}}</h1>
+                                <h6 class="text-white">Total Tickets</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-primary card-inverse">
+                            <div class="box text-center">
+                                <h1 class="font-light text-white">1,738</h1>
+                                <h6 class="text-white">Responded</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-inverse card-success">
+                            <div class="box text-center">
+                                <h1 class="font-light text-white">1100</h1>
+                                <h6 class="text-white">Resolve</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-inverse card-dark">
+                            <div class="box text-center">
+                                <h1 class="font-light text-white">964</h1>
+                                <h6 class="text-white">Pending</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                </div>
+                <a class="btn btn-primary" href="{{ route('ticket.create')}}" role="button"><i class="fa fa-plus-circle"></i> New</a>
 
-<div class="row mt-2">
+
+                <div class="table-responsive">
+                    <table id="demo-foo-addrow" class="table m-t-30 table-hover no-wrap contact-list" data-page-size="10">
+                        <thead>
+                            <tr>
+                                <th>ID #</th>
+                                <th>Title</th>
+                                <th>Opened By</th>
+                                <th>Status</th>
+                                <th>Assign to</th>
+                                <th>Date</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                          @foreach($tickets as $ticket)
+                            <tr>
+                                <td>{{$ticket->id}}</td>
+                                <td><a href="{{ route('ticket.show',$ticket->id)}}"> {{ str_limit($ticket->ticket_title, 35)}}</a> <small class="text-muted"> ({{$ticket->comments()->count()}})</small></td>
+                                <td>
+                                    <a href="javascript:void(0)"><img src="{{Auth::user()->gravatar}}" alt="user" class="img-circle" /> {{$ticket->requested_by_user->name}}</a>
+                                </td>
+                                <td>{{$ticket->category['category_name']}}</td>
+                                <td>{{$ticket->location['location_name']}}</td>
+                                <td>
+                                  @foreach($ticket->user as $ticket_assignee)
+                                    {{$ticket_assignee->name}}
+                                  @endforeach
+                                </td>
+                                <td>{{$ticket->status['status_name']}}</td>
+                                <td>
+                                  <form onsubmit="return confirm('Do you really want to delete?');" action="{{ route('ticket.destroy', $ticket->id)}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <a href="{{ route('ticket.edit',$ticket->id)}}" class="btn btn-primary"><i class="icon ion-md-create"></i></a>
+                                    <a href="{{ route('ticket.show',$ticket->id)}}" class="btn btn-primary"><i class="icon ion-md-eye"></i></a>
+                                    <button class="btn btn-danger" type="submit"><i class="icon ion-md-trash"></i></button>
+                                  </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="6">
+                                    <div class="text-right">
+                                        <ul class="pagination"> </ul>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- old table --}}
+
+
+{{-- <div class="row mt-2">
 <div class="col">
 <div class="card uper">
   <div class="card-header">
@@ -34,24 +132,19 @@
   <table class="table table-striped">
     <thead>
         <tr>
-          {{-- <td>ID</td> --}}
+
           <td>Ticket</td>
-          {{-- <td>ticket content</td> --}}
           <td>Category</td>
           <td>Location</td>
           <td>Agents</td>
           <td>Status</td>
-          @role('admin')
           <td>Action</td>
-          @endrole
         </tr>
     </thead>
     <tbody>
         @foreach($tickets as $ticket)
         <tr>
-            {{-- <td>{{$ticket->id}}</td> --}}
             <td><h4><small class="text-muted">#{{$ticket->id}} </small> <a href="{{ route('ticket.show',$ticket->id)}}"> {{ str_limit($ticket->ticket_title, 35)}}</a> <small class="text-muted"> ({{$ticket->comments()->count()}})</small></h4></td>
-            {{-- <td>{{$ticket->ticket_content}}</td> --}}
             <td>{{$ticket->category['category_name']}}</td>
             <td>{{$ticket->location['location_name']}}</td>
             <td>
@@ -62,9 +155,7 @@
             <td>{{$ticket->status['status_name']}}</td>
             <td>
 
-            {{-- </td>
-            <td> --}}
-            @role('admin')
+
                 <form onsubmit="return confirm('Do you really want to delete?');" action="{{ route('ticket.destroy', $ticket->id)}}" method="post">
                   @csrf
                   @method('DELETE')
@@ -72,7 +163,6 @@
                   <a href="{{ route('ticket.show',$ticket->id)}}" class="btn btn-primary"><i class="icon ion-md-eye"></i></a>
                   <button class="btn btn-danger" type="submit"><i class="icon ion-md-trash"></i></button>
                 </form>
-                @endrole
             </td>
         </tr>
         @endforeach
@@ -82,5 +172,5 @@
 {!! $tickets->links() !!}
 </div>
 </div>
-</div>
+</div> --}}
 @endsection
