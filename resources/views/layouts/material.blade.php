be79a102e5b6f4b14be96de8f478cc84d7f8b19b
omego <mserihi@gmail.com>
2018-11-21 16:27:18 +0300

    style switcher + create permission theme

--- resources/views/layouts/material.blade.php
+++ resources/views/layouts/material.blade.php
@@ -501,13 +501,21 @@
                 <!-- ============================================================== -->
                 <div class="row page-titles">
                     <div class="col-md-5 col-8 align-self-center">
-                        <h3 class="text-themecolor m-b-0 m-t-0">Support Ticket</h3>
+                        <h3 class="text-themecolor m-b-0 m-t-0">Dashboard</h3>
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                            <li class="breadcrumb-item active">Ticket</li>
+                            <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
                     </div>
+                    <div class="col-md-7 col-4 align-self-center">
+                        <div class="d-flex m-t-10 justify-content-end">
 
+
+                            <div class="">
+                                <button class="right-side-toggle waves-effect waves-light btn-success btn btn-circle btn-sm pull-right m-l-10"><i class="ti-settings text-white"></i></button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <!-- ============================================================== -->
                 <!-- End Bread crumb and right sidebar toggle -->
--- resources/views/permissions/create.blade.php
+++ resources/views/permissions/create.blade.php
@@ -1,31 +1,34 @@
-@extends('layouts.app')
+@extends('layouts.material')
 
 @section('content')
 
-<div class = 'container'>
-<div class="card uper">
-  <div class="card-header">
-    Add Permission
-  </div>
-  <div class="card-body">
-    @if ($errors->any())
-      <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-      </div><br />
-    @endif
-      <form method="post" action="{{ route('permissions.store') }}">
-          <div class="form-group">
-              @csrf
-              <label for="name">Permission name</label>
-							<input type="text" name = "name" class = "form-control" placeholder = "Name">
-          </div>
+  @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+      </ul>
+    </div><br />
+  @endif
 
-          <button type="submit" class="btn btn-primary">Create</button>
-      </form>
+  <div class="row">
+      <div class="col-12">
+          <div class="card">
+              <div class="card-body">
+                <h4 class="card-title">Add new Permissions</h4>
+                <form method="post" action="{{ route('permissions.store') }}">
+                    <div class="form-group">
+                        @csrf
+                        <label for="name">Permission name</label>
+                        <input type="text" name = "name" class = "form-control" placeholder = "Name">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Create</button>
+                </form>
+              </div>
+          </div>
+      </div>
   </div>
-</div></div>
+
 @endsection
