6bfb9cc42616a42f37ed8ad3f776b41475e608c1
omego <mserihi@gmail.com>
2018-11-21 12:11:18 +0300

    Asset fix

--- resources/views/layouts/material.blade.php
+++ resources/views/layouts/material.blade.php
@@ -602,29 +602,29 @@
 <!-- ============================================================== -->
 <!-- All Jquery -->
 <!-- ============================================================== -->
-<script src="../assets/plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap tether Core JavaScript -->
-<script src="../assets/plugins/popper/popper.min.js"></script>
-<script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="{{ asset('assets/plugins/popper/popper.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
 <!-- slimscrollbar scrollbar JavaScript -->
-<script src="js/jquery.slimscroll.js"></script>
+<script src="{{ asset('js/jquery.slimscroll.js') }}"></script>
 <!--Wave Effects -->
-<script src="js/waves.js"></script>
+<script src="{{ asset('js/waves.js') }}"></script>
 <!--Menu sidebar -->
-<script src="js/sidebarmenu.js"></script>
+<script src="{{ asset('js/sidebarmenu.js') }}"></script>
 <!--stickey kit -->
-<script src="../assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
-<script src="../assets/plugins/sparkline/jquery.sparkline.min.js"></script>
+<script src="{{ asset('assets/plugins/sticky-kit-master/dist/sticky-kit.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
 <!--Custom JavaScript -->
-<script src="js/custom.min.js"></script>
+<script src="{{ asset('js/custom.min.js') }}"></script>
 <!-- Footable -->
-<script src="../assets/plugins/footable/js/footable.all.min.js"></script>
-<script src="../assets/plugins/bootstrap-select/bootstrap-select.min.js" type="text/javascript"></script>
+<script src="{{ asset('assets/plugins/footable/js/footable.all.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/bootstrap-select/bootstrap-select.min.js') }}" type="text/javascript"></script>
 <!--FooTable init-->
-<script src="js/footable-init.js"></script>
+<script src="{{ asset('js/footable-init.js') }}"></script>
 <!-- ============================================================== -->
 <!-- Style switcher -->
 <!-- ============================================================== -->
-<script src="../assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+<script src="{{ asset('assets/plugins/styleswitcher/jQuery.style.switcher.js') }}"></script>
 </body>
 </html>
