83968dc9cbe3f1c4ee8caaad8d12a980b40e72a7
omego <mserihi@gmail.com>
2018-12-06 10:38:20 +0300

    Room number and group id migrations

---
+++ database/migrations/2018_12_06_071008_add_room_number_to_tickets.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRoomNumberToTickets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            //
+            $table->string('room_number')->after('requested_by');
+            $table->string('priority')->after('room_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            //
+            $table->dropColumn('room_number');
+            $table->dropColumn('priority');
+        });
+    }
+}
