ee05821cc9eae751555e41400d1ad0d1c376d262
omego <mserihi@gmail.com>
2018-12-16 16:35:45 +0300

    ticket show design init

--- resources/views/ticket/show.blade.php
+++ resources/views/ticket/show.blade.php
@@ -14,6 +14,132 @@
       {{ session()->get('success') }}
     </div><br />
   @endif
+
+
+                          <div class="row">
+                            <div class="col-md-12">
+                            <div class="card">
+
+
+                                <div class="card-body">
+                                  <div class="row">
+                                      <div class="col-md-8">
+                                        <h3 class="card-title">{{title_case($tickets->ticket_title)}} <small class="text-muted"> in {{$tickets->location->location_name}} {{$ticket->room_number}}</small></h3>
+                                        <h6 class="card-subtitle mb-2 text-muted">Created by {{$tickets->created_by_user->name}} requested by {{$tickets->requested_by_user->name}} {{$tickets->created_at->diffForHumans() }}</h6>
+</div>
+                                      <div class="col-md-4 text-right">
+                                        <!-- Example single danger button -->
+                                        @can('change ticket status')
+                                           <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                             {{$tickets->status->status_name}}
+                                           </button>
+                                           <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                           @foreach ($statuses as $key => $value)
+                                             {{-- <button class="dropdown-item" type="button">{{$key}}{{$value}}</button> --}}
+                                             <a class='dropdown-item' href='{{url('ticket/ChangeTicketStatus')}}/{{$key}}/{{$tickets->id}}'>{{$value}}</a>
+                                           @endforeach
+                                           </div>
+                                        @endcan
+
+     <div class="btn-group">
+
+       <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         Action
+       </button>
+       <div class="dropdown-menu dropdown-menu-right">
+         @can('update ticket')<a class="btn" href="{{ route('ticket.edit',$tickets->id)}}" role="button">Edit</a>@endcan
+         {{-- <a class="dropdown-item" href="#">Another action</a>
+         <a class="dropdown-item" href="#">Something else here</a> --}}
+         @can('delete ticket')
+         <div class="dropdown-divider"></div>
+         <form onsubmit="return confirm('Do you really want to delete?');" action="{{ route('ticket.destroy', $ticket->id)}}" method="post">
+           @csrf
+           @method('DELETE')
+           <button class="btn" type="submit">Delete</button>
+         </form>
+         @endcan
+       </div>
+     </div>
+                                      </div>
+                                  </div>                                </div>
+                                <div class="card-footer text-muted">
+                                  <span class="badge badge-warning">Assigned to
+                                    @foreach($tickets->user as $ticket_assignee)
+                                      {{$ticket_assignee->name}}
+                                    @endforeach
+                                  </span>
+                                  <span class="badge badge-warning">{{$tickets->status->status_name}}</span> <span class="badge badge-warning"> {{$ticket->priority}} </span> <span class="badge badge-warning"> {{$ticket->group->group_name}} </span>
+                            </div>
+                            </div>
+                          </div>
+                          </div>
+
+
+
+
+
+                          {{-- <div class="row">
+                            <div class="col-md-12 text-center">
+
+                                <a class="btn btn-circle btn-lg btn-success"><i class="fa fa-link"></i></a>
+                                <a class="btn btn-circle btn-lg btn-success"><i class="fa fa-link"></i></a>
+                                <a class="btn btn-circle btn-lg btn-success"><i class="fa fa-link"></i></a>
+                                <a class="btn btn-circle btn-lg btn-success"><i class="fa fa-link"></i></a>
+
+                          </div>
+                          </div> --}}
+
+                          {{-- <div class="row">
+                            <div class="col-md-12 text-center">
+
+                                <button type="button" class="btn btn-success"><i class="fa fa-check"></i> Success</button>
+                                <button type="button" class="btn btn-success"><i class="fa fa-check"></i> Success</button>
+                                <button type="button" class="btn btn-success"><i class="fa fa-check"></i> Success</button>
+                                <button type="button" class="btn btn-success"><i class="fa fa-check"></i> Success</button>
+
+                          </div>
+                          </div> --}}
+
+
+                          <div class="row">
+                            <div class="col-md-12">
+                            <div class="card">
+
+                                <div class="card-body">
+                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                </div>
+                            </div>
+                          </div>
+                          </div>
+
+
+                          <div class="row">
+                            <div class="col-md-12">
+                            <div class="card">
+
+                                <div class="card-body">
+<h4 class="card-title">Comments</h4>
+
+                                  @include('comments._comment_replies', ['comments' => $tickets->comments, 'ticket_id' => $tickets->id])
+
+                                  <hr />
+                              <h4>Add comment</h4>
+                              <form method="post" action="{{ route('comment.add') }}">
+                                  @csrf
+                                  <div class="form-group">
+                                      <input type="text" name="comment_body" class="form-control" />
+                                      <input type="hidden" name="ticket_id" value="{{ $tickets->id }}" />
+                                  </div>
+                                  <div class="form-group">
+                                      <input type="submit" class="btn btn-dark" value="Add Comment" />
+                                  </div>
+                                </div>
+                            </div>
+                          </div>
+                          </div>
+
+
+
 <div class="card uper">
   <div class="card-header">
     <form onsubmit="return confirm('Do you really want to delete?');" action="{{ route('ticket.destroy', $ticket->id)}}" method="post">
