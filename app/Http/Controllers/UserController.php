6b86366fa930d2d18f012e948c2ea3139a19453c
Ssy3 <ssy.vip@gmail.com>
2019-02-17 12:02:05 +0300

    users orderby created at DESC

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
     public function index()
     {
         //
-        $users = User::paginate(10);
+        $users = User::orderByRaw('created_at DESC')->paginate(10);
         return view('users.index', compact('users'));
     }
 
