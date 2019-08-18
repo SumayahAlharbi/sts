568fe7d003a4e59083573c7c776c1212e5d92c28
ManalNawar <manal.s.nawar@gmail.com>
2019-08-18 13:23:08 +0300

    Rigester CAS users as End-Users

--- app/LaravelUserMapper.php
+++ app/LaravelUserMapper.php
@@ -5,6 +5,7 @@ namespace App;
 use App\User;
 use Auth;
 Use Exception;
+use Illuminate\Support\Facades\Hash;
 
 class LaravelUserMapper
 {
@@ -20,10 +21,39 @@ class LaravelUserMapper
                 // Validate the value...
                 $user = User::where('email', $CasUserFullEmail)->firstOrFail();
                 Auth::login($user);
+                //if (Auth::user()->role->isEmpty()) {
+                //     $user()->addRole('enduser');
+                //     Auth::login($user);
+                // }
+                // else{
+                    
+                // }
+                
             } catch (Exception $e) {
-                report($e);
 
-                return false;
+                //user not found in the databse..register new user from cas
+                $user = new User;
+                $user->name= $username;
+                $user->email= $username."@ksau-hs.edu.sa";
+                $user->password= Hash::make('the-password-of-choice');
+                $user->save();
+
+                //all new users  gets enduser role
+                $user->assignRole('enduser');
+
+                // these permissions required for end-user to access and create tickets
+                $user->givePermissionTo('show ticket');
+                $user->givePermissionTo('view tickets list');
+                $user->givePermissionTo('End User Create Ticket');
+                
+                //report($e);
+
+                //return false;
+                Auth::login($user);
+            
+                
+
+                
             }
 
 
