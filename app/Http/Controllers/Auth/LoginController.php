3efc376c6836f8d4a6c95ce7881418a26ae41251
omego <mserihi@gmail.com>
2019-10-30 22:47:09 +0300

    remove Socialite

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Socialite;
 use App\User;
 use Auth;
 use Illuminate\Support\Facades\Hash;
