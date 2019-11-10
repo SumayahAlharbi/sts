45256f961ffef1ecceafc156d426956241e0965b
SumayahAlharbi <ftooo-ff@hotmail.com>
2019-11-10 15:48:42 +0300

    Revert "Prevent sending rating email if the ticket requested by user id = assigned agent id"
    This reverts commit c3f85631a2af5c570baf8ed0c42e11be02865204.

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -401,14 +401,7 @@ class TicketController extends Controller
       $ticket = Ticket::findorfail($ticket_id);
       $user = User::find($ticket->requested_by_user);
 
-      $TicketAgents = $ticket->user;
-      $match = 1;
-      foreach ($TicketAgents as $TicketAgent){
-        if ($user[0]->id == $TicketAgent->id)
-        $match = 0;
-      }
-
-      if ($user && $match) {
+      if ($user) {
         if (App::environment('production')) {
             // The environment is production
             \Mail::to($user)->send(new TicketRating($ticket));
