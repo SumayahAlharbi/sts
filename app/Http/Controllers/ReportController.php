7d926c5929c9e5f489eaea9d3a2fc04c1447a4ed
ManalNawar <manal.s.nawar@gmail.com>
2020-01-08 12:14:32 +0300

    fixed:columns in csv

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -103,7 +103,6 @@ public function displayReport(Request $request)
     //app('debugbar')->disable();
     $fromDate = Carbon::parse($request->input('from_date'))->startOfDay();
     $toDate = Carbon::parse($request->input('to_date'))->endOfDay();
-    $sortBy = $request->input('sort_by');
     $agentId = $request->input('user_id');
 
     // $users = User::with(['roles' => function($q){
@@ -119,22 +118,29 @@ public function displayReport(Request $request)
 
     $meta = [ // For displaying filters description on header
         'Tickets from' => $fromDate . ' To ' . $toDate,
-        //'Sort By' => $sortBy
+        'user_id' => $agentId
     ];
 
-      $queryBuilder = Ticket::select(['id', 'ticket_title', 'created_at', 'location_id', 'created_at', 'status_id', 'category_id']) // Do some querying..
+      $queryBuilder = Ticket::select(['id', 'ticket_title', 'created_at', 'location_id', 'created_at', 'status_id', 'category_id','requested_by']) // Do some querying..
                           ->whereBetween('created_at', [$fromDate, $toDate])
                           ->where('status_id','=',1)
                           ->with(['user' => function ($query) {
                             $query->where('id', '=', $agentId);
                         }])->with(['rating' => function ($query) {
                             $query->select('rating_value');
-                        }])->orderBy('id');
+                        }])->with(['requested_by_user' => function ($query) {
+                            $query->where('id', '=', 'requested_by');
+                        }])
+                        ->orderBy('id');
 
 
     $columns = [ // Set Column to be displayed
         'ID' => 'id',
         'Title' => 'ticket_title',
+        'requested_by' => function($queryBuilder) {
+            return $queryBuilder->requested_by_user->name;
+          },
+        'created_at' => 'created_at',
         'Agent' => function($queryBuilder) {
             $date = array();
             foreach ($queryBuilder->user as $Builder) {
@@ -143,16 +149,24 @@ public function displayReport(Request $request)
             // return json_encode($date);
             return implode(', ', $date);
           },
-        'Status' => function($queryBuilder) {
-            return $queryBuilder->status->status_name;
-          },
           'Rating' => function($queryBuilder) {
             return $queryBuilder->rating->rating_value;
           }
     ];
+    $rows =[
+
+    ];
 
     CSVReport::of($title, $meta, $queryBuilder, $columns)
-             // ->withoutManipulation()
+            ->editColumn('created_at', [ // Change column class or manipulate its data for displaying to report
+                'displayAs' => function($result) {
+                    return $result->created_at->format('d M Y');
+                },
+                'class' => 'center'
+                ])
+            ->showTotal([
+                'Rating' => 'point'
+                ])
              ->showNumColumn(false)
              ->download($filename);
            }
--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -3,57 +3,57 @@
 
 @section('content')
 
-<div class = 'container'>
+<div class='container'>
 
   @if(session()->get('success'))
-    <div class="alert alert-success">
-      {{ session()->get('success') }}
-    </div><br />
+  <div class="alert alert-success">
+    {{ session()->get('success') }}
+  </div><br />
   @endif
 
-<button type="button" class="btn btn-link"></button>
+  <button type="button" class="btn btn-link"></button>
 
 
-<div class="row">
+  <div class="row">
     <div class="col-12">
-<div class="card">
+      <div class="card">
 
-  <div class="card-body">
-<h4 class="card-title">Data Generator v0.0.1</h4>
-
-      <form method="post" action="{{url('report/display')}}">
-        <div class="row">
-          <div class="form-group col-md-4">
-              @csrf
-              <label for="from_date">from</label>
-              <input type="date" class="form-control" name="from_date" />
-          </div>
-          <div class="form-group col-md-4">
-            <label for="to_date">to</label>
-            <input type="date" class="form-control" name="to_date" />
-          </div>
-          <div class="col-md-4">
-            <div class="form-group">
-            <label for="name">Agent list</label>
-              <select name="user_id" id="" data-show-subtext="true" data-live-search="true" class="selectpicker form-control">
-                <option selected disabled value> -- Choose an Agent -- </option>
-                  @foreach($agentUsers as $agentUser)
-                  <option value="{{$agentUser->id}}">{{$agentUser->name}}</option>
-                  @endforeach
-              </select>
+        <div class="card-body">
+        <h4 class="card-title">Reports</h4>
+        <p class="card-text">generate a report for all completed tickets by the agent and its rating</p>
+          <form method="post" action="{{url('report/display')}}">
+            <div class="row">
+              <div class="form-group col-md-4">
+                @csrf
+                <label for="from_date">from</label>
+                <input type="date" class="form-control" name="from_date" />
+              </div>
+              <div class="form-group col-md-4">
+                <label for="to_date">to</label>
+                <input type="date" class="form-control" name="to_date" />
+              </div>
+              <div class="col-md-4">
+                <div class="form-group">
+                  <label for="name">Agent list</label>
+                  <select name="user_id" id="" data-show-subtext="true" data-live-search="true" class="selectpicker form-control">
+                    <option selected disabled value> -- Choose an Agent -- </option>
+                    @foreach($agentUsers as $agentUser)
+                    <option value="{{$agentUser->id}}">{{$agentUser->name}}</option>
+                    @endforeach
+                  </select>
+                </div>
+              </div>
             </div>
-          </div>
+            <button type="submit" class="btn btn-primary">Generate & Download</button>
+          </form>
+
+
+
+
+
         </div>
-        <button type="submit" class="btn btn-primary">Generate & Download</button>
-      </form>
-
-
-
-
-
-</div>
-</div>
-</div>
-</div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
