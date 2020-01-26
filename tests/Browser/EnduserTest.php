4f7b224567dbe8006452f2d16f47301b3209aa71
ManalNawar <manal.s.nawar@gmail.com>
2020-01-26 16:08:50 +0300

    added:TestenduserCreatTicket

--- tests/Browser/EnduserTest.php
+++ tests/Browser/EnduserTest.php
@@ -22,12 +22,33 @@ class EnduserTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->visit('/login');
             $browser->clickLink('Admin');
-            $browser->type('email', 'enduser@enduser.com');
-            $browser->type('password', '123123');
+            $browser->type('email', env('TEST_ENDUSER_ROLE_USER'));
+            $browser->type('password', env('TEST_ENDUSER_ROLE_PASSWORD'));
             // $browser->pause(3000);
             $browser->press('LOGIN');
             // $browser->pause(3000);
             $browser->assertPathIs('/');
+        });
+    }
+
+    public function testEnduserCreateTicket()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/ticket');
+            $browser->visit('/ticket');
+            $browser->visit('/ticket/create');
+            $browser->type('ticket_title', 'test ticket ' . Carbon::now()->toDateTimeString());
+            //$browser->select('High', 'priority');
+            $browser->script("CKEDITOR.instances['contentEditor'].setData('Test Data');");
+            $browser->pause(3000);
+            $browser->select('region','1');
+            $browser->select('group_id','1');
+            $browser->select('location_id','1');
+            $browser->select('category_id','2');
+            $browser->type('room_number','324');
+            $browser->press('Create')
+                    ->assertSee('Comments')
+                    ->screenshot('home-page');
         });
     }
 }
