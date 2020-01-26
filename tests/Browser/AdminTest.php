83581d3b6653f0d621d933f27dbef3f7105dce4f
SumayahAlharbi <ftooo-ff@hotmail.com>
2020-01-26 15:16:46 +0300

    Added testAdminCreateTicket

--- tests/Browser/AdminTest.php
+++ tests/Browser/AdminTest.php
@@ -5,6 +5,7 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Carbon\Carbon;
 
 class AdminTest extends DuskTestCase
 {
@@ -24,4 +25,28 @@ class AdminTest extends DuskTestCase
           $browser->assertSee('ADMINISTRATOR');
       });
     }
+
+    public function testAdminCreateTicket()
+    {
+      $this->browse(function (Browser $browser) {
+          $browser->visit('/ticket');
+          $browser->waitFor('.create-ticket-button');
+          $browser->clickLink('Create Ticket');
+          $browser->visit('/ticket/create');
+          $browser->type('ticket_title', 'test ticket ' . Carbon::now()->toDateTimeString());
+          $browser->script("CKEDITOR.instances['contentEditor'].setData('Test Data');");
+          $browser->pause(3000);
+          $browser->select('region','1');
+          $browser->waitFor('.group');
+          $browser->select('group_id','1');
+          $browser->waitFor('#locationDiv');
+          $browser->select('location_id', '1');
+          $browser->waitFor('#categoryDiv');
+          $browser->select('category_id', '1');
+          $browser->press('Create')
+                  ->assertSee('Ticket has been created')
+                  ->screenshot('create_ticket'); // not needed
+      });
+    }
+
 }
